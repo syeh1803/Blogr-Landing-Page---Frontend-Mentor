e4fa9a1a4442110ee40462bae63ac82c4eb624d7
syeh1803 <79744679+syeh1803@users.noreply.github.com>
2021-11-22 17:05:54 +0800

    feat: add header

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,39 @@
-console.log('hello');
+const burger = document.querySelector("#burger");
+const close = document.querySelector("#close");
+const navList = document.querySelector("#nav-list");
+
+// burger icon toggle
+burger.addEventListener("click", () => {
+  navList.classList.toggle("show");
+  burger.style.display = "none";
+  close.style.display = "block";
+});
+
+// close icon toggle
+close.addEventListener("click", () => {
+  navList.classList.toggle("show");
+  close.style.display = "none";
+  burger.style.display = "block";
+});
+
+// accordion
+document.querySelectorAll("#arrow-icon").forEach((arrow) => {
+  // mobile
+  arrow.addEventListener("click", () => {
+    arrow.classList.toggle("open");
+    arrow.parentElement.nextElementSibling.classList.toggle("show");
+  });
+
+  // desktop
+  arrow.addEventListener("mouseover", () => {
+    arrow.classList.toggle(":hover");
+    arrow.parentElement.nextElementSibling.classList.add("show");
+  });
+});
+
+document.querySelectorAll("#nav-dropdown").forEach((dropdown) => {
+  // desktop
+  dropdown.addEventListener("mouseleave", () => {
+    dropdown.classList.remove("show");
+  });
+});
